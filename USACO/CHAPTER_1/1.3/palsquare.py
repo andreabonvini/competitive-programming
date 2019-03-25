4532fd104f391610bceffb6aeb9cfee2544813c1
Andrea Bonvini <a.bonvini96@gmail.com>
2019-03-25 12:10:21 +0100

    palsquare.py added

--- USACO/CHAPTER_1/1.3/palsquare.py
+++ USACO/CHAPTER_1/1.3/palsquare.py
@@ -4,7 +4,7 @@ LANG: PYTHON3
 PROG: palsquare
 """
 
-dic = {1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9',10:'A',11:'B',12:'C',13:'D',14:'E',15:'F',16:'G',17:'H',18:'I',19:'L',20:'M'}
+dic = {0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9',10:'A',11:'B',12:'C',13:'D',14:'E',15:'F',16:'G',17:'H',18:'I',19:'L',20:'M'}
 fin = open('palsquare.in', 'r')
 fout = open('palsquare.out', 'w')
 B = int(fin.readline().strip())
@@ -14,7 +14,7 @@ def conv(B,n):
 	while q != 0:
 		x.append(dic[q%B])
 		q //=B
-	x = sorted(x,reverse = True)
+	x = x[::-1]
 	return ''.join(x)
 def is_pal(x):
 	if x == x[::-1]:
